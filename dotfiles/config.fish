5f9e9a91b65fa4ba409656df82308b6e18aed832
Hamilton Little <hamilton.little@hayden.ai>
2025-08-04 18:32:32 -0700

    chore: add comments to config.fish

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -1,19 +1,28 @@
+## Interactive login settings ##################################################
 if status is-interactive
     set -gx EDITOR code
     theme_gruvbox dark hard
     fish_vi_key_bindings
 end
 
+
+## PATH stuff ##################################################################
+# local
 fish_add_path "$HOME/devtools/bin"
 fish_add_path "$HOME/devtools/scripts"
 
+# go
 fish_add_path "/usr/local/go/bin"
 fish_add_path "$HOME/go/bin"
 
+# rust
 source "$HOME/.cargo/env.fish"
 
+# other
 zoxide init fish | source
 
+
+## Aliases #####################################################################
 alias lgi='lazygit'
 
 function y
